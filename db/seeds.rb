329cd790693e11aea96b1bff47f0be1d3746c036
Debjani Banerjee <dbjnbnrj@gmail.com>
2015-10-18 11:48:47 -0700

    Initial Search Features

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-p1 = Product.create([{name: 'Sample1', 
-					origin:'http://www.amazon.com/B001AMHWP8',
-    				asin:'B001AMHWP8',
-    				rating:4.5,
-    				info:"Macbook Air",
-    				price:1099},
-    				{name: 'Sample2', 
-					origin:'http://www.amazon.com/B001AMHWP8',
-    				asin:'B001AMHWP10',
-    				rating:4.5,
-    				info:"Windows XP",
-    				price:1099},
-    				{name: 'Sample3', 
-					origin:'http://www.amazon.com/B001AMHWP8',
-    				asin:'B001AMHWP8',
-    				rating:5.0,
-    				info:"Macbook Air",
-    				price:899}])
+
+photo = File.new("#{Rails.root}/app/assets/images/default.jpeg")
+
+desc1 = 'This is a 16.1 MP Digital Camera with 60x Optical Image Stabilized Zoom and 3 inch LCD. It offers full 1080p HD video recording with auto focus and has realistic Dolby Digital quality sound recording for enhanced audio quality. It comes with 15 filter effects that can be used on your photos and video.'
+desc2 = 'This is a 20.1 MP Digital Camera with a 2.7 inch LCD. It can record 720p HD movies. It has face detection technology that detects up to eight individual faces and adjusts focus, exposure and white balance to help deliver crisp, properly lit images of family and friends.'
+desc3 = 'This is a 12.1 MP Digital Camera with 30x optical zoom and 1080p full HD video. It has built in Wi-Fi technology for on-the-go convenience when you want to share your photos and videos with friends, family and the whole world.'
+desc4 = 'This is an instant film camera. It automatically detects the brightness of the surrounding and adjusts the amount of flash and shutter speed to optimize photo quality. It has kids mode suitable for photographing kids, pets and other fast moving subjects due to a fast shutter speed.'
+
+Product.create! :name => 'Panasonic LUMIX DMC-FZ70', :price => 320.00, :description => desc1, :photo => photo
+Product.create! :name => 'Sony DSC W830/B', :price => 140.00, :description => desc2, :photo => photo
+Product.create! :name => 'Canon PowerShot SX510 HS', :price => 230.00, :description => desc3, :photo => photo
+Product.create! :name => 'Fujifilm Instax Mini 90 Neo Classic Instant Film Camera', :price => 150.00, :description => desc4, :photo => photo
