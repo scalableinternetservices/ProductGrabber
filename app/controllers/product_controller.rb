3f3bd15b60e88353ea038741b2c20ca43c93d02a
Debjani Banerjee <dbjnbnrj@gmail.com>
2015-10-17 19:12:07 -0700

    Integrating Amazon and Product View Code

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,5 +1,7 @@
 class ProductController < ApplicationController
 	def index
-		@products = Product.all
+		fet = ProductHelper::AmazonProductQuery.new("fasthall-20")
+		fet.set_credential("AKIAJIO5ZZVUB6DZXK4Q", "6DjHhRTJbHLcuWz6oB6ehTNXffTW+lbrpgooEs1q")
+        @products = fet.search("laptop")
 	end
 end
--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -51,12 +51,17 @@ module ProductHelper
             result = Nokogiri::XML(open(generate_url))
             items = []
             result.css("Item").each do |i|
+                puts i
                 item = AmazonProduct.new
                 item.asin = i.css("ASIN").text
                 item.title = i.at_css("ItemAttributes/Title").text
                 item.url = i.at_css("DetailPageURL").text
                 item.image = [i.at_css("SmallImage/URL").text, i.at_css("MediumImage/URL").text, i.at_css("LargeImage/URL").text]
-                item.price = i.at_css("LowestNewPrice/FormattedPrice").text
+                if i.at_css("LowestNewPrice/FormattedPrice") == nil
+                    item.price = "N/A"
+                else
+                            item.price = i.at_css("LowestNewPrice/FormattedPrice").text
+                end
                 items << item
             end
             items
