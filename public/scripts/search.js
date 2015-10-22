b86a89ee9571e70345ee83a507bcca258268409f
Darshan <darshanmaiya@outlook.com>
2015-10-22 15:03:26 -0700

    Search Integration with UI

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,13 +1,8 @@
 requirejs([], function() {
 	
 	var url = location.href;
-	var searchQuery = url.substring(url.search("q=") + 2);
+	var searchQuery = url.substring(url.search("search=") + 7);
 	var searchResults = "Search results for \"" + searchQuery + "\"";
 	$("title").text(searchResults);
-	$("#search-results-text").text(searchResults);
-	
-	if(searchQuery === "laptops") {
-		$("#laptop-results").removeClass("hide");
-		$("#mobile-results").addClass("hide");
-	}
+	$("#search-results-text").val(searchQuery);
 });
