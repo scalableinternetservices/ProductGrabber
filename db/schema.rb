9ceef81adb27f4985e7c6e25fe56f7be48381b8b
7johnson <xbinhan@hotmail.com>
2015-11-17 10:01:37 -0700

    alter seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111004152) do
+ActiveRecord::Schema.define(version: 20151105205103) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20151111004152) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
   end
 
   create_table "favorites", force: :cascade do |t|
