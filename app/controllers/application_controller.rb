5f9cf3d101a784e4b3431db5133f33c9cf38b440
7johnson <xbinhan@hotmail.com>
2015-11-16 19:00:08 -0700

    skip authentity_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ include SessionsHelper
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
 
   def logged_in_user
     unless logged_in?
