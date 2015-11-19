1fd2ad9388242b0afb4b7972dcb97e098a4e164b
Binhan Xu <xbinhan@hotmail.com>
2015-11-19 10:54:56 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
+  before_action :logged_in_user
 
   def logged_in_user
     unless logged_in?
