95e42abb44024a6f6ee9711242499ccd14ec851d
Debjani Banerjee <dbjnbnrj@gmail.com>
2015-12-01 09:06:47 -0800

    Create products with images

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,10 @@ class Product < ActiveRecord::Base
   has_many :favorites
   before_destroy :ensure_not_referenced_by_any_favorite
 
-  has_attached_file :photo
+  has_attached_file :photo, 
+          :url  => "/images/:style/:id/:basename.:extension",
+          :path => ":rails_root/public/images/:basename.:extension"
+
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   validates :name, presence: true
